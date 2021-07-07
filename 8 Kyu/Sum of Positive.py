2e7ba487167d5a7e37462b67410608e6fa61d326
Oryto Mikli <72039670+Rianto21@users.noreply.github.com>
2021-07-07 21:33:38 +0700

    Create Sum of Positive.py

---
+++ 8 Kyu/Sum of Positive.py
@@ -0,0 +1,14 @@
+"""
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+"""
+
+def positive_sum(arr):
+    sum = 0
+    for i in arr:
+        if i > 0:
+            sum += i
+    return sum
